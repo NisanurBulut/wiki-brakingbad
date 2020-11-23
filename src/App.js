9f9a06cae182473667524d0f60af5de5bb6d5e42
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-23 20:41:51 +0300

    using the api and get data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header';
 import './App.css';
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-function App() {
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios('https://www.breakingbadapi.com/api/characters');
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+const header = () => {
+  return (
+    <header className="center">
+      <img src={logo} alt=""></img>
+    </header>
+  );
+};
+
+export default header;
