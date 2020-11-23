08435efa9073f63194630495b7580676929fef1d
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-23 21:21:37 +0300

    searching completed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Header from './components/ui/Header';
-import Search from './components/ui/Search';
-import CharacterGrid from './components/characters/CharacterGrid';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
+import './App.css'
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [query, setQuery] = useState('');
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
   useEffect(() => {
     const fetchItems = async () => {
+      setIsLoading(true)
       const result = await axios(
-        'https://www.breakingbadapi.com/api/characters?name=${query}'
-      );
-      setItems(result.data);
-      setIsLoading(false);
-    };
-    fetchItems();
-  }, [query]);
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
+
+      // console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
-      <Search getQuery={(q)=>setQuery(q)}/>
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import CharacterItem from './CharacterItem';
+import Spinner from '../ui/Spinner';
 
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
-    <h1>...Loading</h1>
+    <h1><Spinner/></h1>
   ) : (
     <section className="cards">
       {items.map((item) => (
-        <CharacterItem id={item.char_id} item={item} />
+        <CharacterItem key={item.char_id} item={item}></CharacterItem>
       ))}
     </section>
   );
